ca408996e2ce62e009d43f9d2f3abe1f97c40153
Paola Anaya <p.andrea.a93@gmail.com>
2026-09-24 15:57:38 -0600

    New work added

---
+++ src/components.ts
@@ -0,0 +1,7 @@
+export { About } from './app/components/About';
+export { Contact } from './app/components/Contact';
+export { Experience } from './app/components/Experience';
+export { Hero } from './app/components/Hero';
+export { LanguageSwitcher } from './app/components/LanguageSwitcher';
+export { Projects } from './app/components/Projects';
+export { Button, buttonVariants } from './app/components/ui/button';
